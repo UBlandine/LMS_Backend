d3f91c3f66448f72d65a2c4c16aab495c4e172c8
UBlandine <ugirablando@gmail.com>
2024-01-30 10:27:20 +0200

    backend-side

--- models/user.models.js
+++ models/user.models.js
@@ -8,5 +8,6 @@ const userSchema = new mongoose.Schema({
     ], required: [true, "Email is required!"],},
   password: {type: String, required: true},
   address: { type: String, required: true},
+ telephone: {type: String, required: true},
 });
 module.exports = mongoose.model('user', userSchema);
--- server.js
+++ server.js
@@ -2,14 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const colors = require('colors');
-
 const allRoutes = require('./routes');
 const app = express();
 
 
 app.use(express.json());
+app.use('/api/sms/v1', allRoutes);+
 
-app.use('/api/sms/v1', allRoutes);
 
 app.listen(process.env.PORT, () => {
     mongoose.connect(process.env.MONGODB_URL)
